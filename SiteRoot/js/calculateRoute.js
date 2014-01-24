c2116aafc889171e8149a9db6f827bebe6c5c72e
Brent <bwmello@alaska.edu>
2014-01-24 06:45:12 -0600

    Changed ReadMe format; minor cosmetic changes to calculateRoute.js.

--- SiteRoot/js/calculateRoute.js
+++ SiteRoot/js/calculateRoute.js
@@ -1,8 +1,11 @@
 /* calculateRoute.js
  * Given one or two locations
  * author Brent Mello
- * lasted edited 1/22/2014
+ * lasted edited 1/24/2014
  */
+
+var allPntCoords;
+var strtLnDist;
 
 function calcRoute() {
     setInitialRoute();
@@ -37,9 +40,10 @@ function drawCurrRoute() {
  *  |   getCoordinates->setSearchArea->findStores->slurpyRoute                  |
  *  V   possible cosmetic alternatives: deferreds, queue                        V
  */
+var startCoords = new google.maps.LatLngBounds();
+var endCoords = new google.maps.LatLngBounds();
+    
 function getCoordinates(setSearchAreaCB) {
-    var startCoords = new google.maps.LatLngBounds();
-    var endCoords = new google.maps.LatLngBounds();
     geocoder.geocode( {'address':directionsRequest.origin}, function(results, status){
         if(status === google.maps.GeocoderStatus.OK)
             startCoords = results[0].geometry.location;
@@ -59,6 +63,7 @@ function getCoordinates(setSearchAreaCB) {
 
 // TODO: the less distance between start and end, the greater the added search area.
 function setSearchArea(startCoords, endCoords, findStoresCB) { //Ugh, revisit later.
+    strtLnDist = totalCoordsDiff(startCoords, endCoords);
     var topMost, botMost, rightMost, leftMost;
     if(startCoords.lat() >= endCoords.lat()) {  //1D = 69m, so .01D = .69m
         topMost = startCoords.lat()+.01; botMost = endCoords.lat()-.01; }
@@ -82,8 +87,8 @@ function findStores(searchArea, slurpyRouteCB) {
         if (status === google.maps.places.PlacesServiceStatus.OK) {
             for (var i=0; i<results.length && i<8; i++) {
                 allStores.push({
-                    location: results[i].geometry.location,
-                    //stopover: false //Change if you'd rather work with suggested routes
+                    location: results[i].geometry.location
+                    //stopover: false //Change to "true" if you'd rather work with suggested routes
                 });
             };
             slurpyRouteCB(allStores);
@@ -96,11 +101,10 @@ function findStores(searchArea, slurpyRouteCB) {
 function slurpyRoute(allStores) {
     directionsRequest.waypoints = allStores;
     directionsRequest.optimizeWaypoints = true;
-    drawCurrRoute();
-    /*if(allStores.length > 1)
+    if(allStores.length > 1)
         prepRoute();
     else
-        drawCurrRoute();*/
+        drawCurrRoute();
 }
 
 
@@ -109,53 +113,29 @@ function slurpyRoute(allStores) {
  * | Culls points based on distance and direction, formula explained in ReadMe      |
  * V                                                                                V
  */
-var allPoints;
-var allPointsCoords;
-var strtLnDist;
 
 function prepRoute() {
-    allPoints = [directionsRequest.origin];
-    alert("In prepRoute(), allPoints[0]: " + allPoints[0]);
+    allPntCoords = [startCoords];
     for(i=0; i<directionsRequest.waypoints.length; i++) {
-        allPoints.push(directionsRequest.waypoints[i]);
+        allPntCoords.push(directionsRequest.waypoints[i].location);
     }
-    allPoints.push(directionsRequest.destination);
-    allPointsCoords = []; //Populated by cnvtToCoords()
-    geocoder = new google.maps.Geocoder();
-    cnvtToCoords(0);
+    allPntCoords.push(endCoords);
+    optmzeRoute();
 }
 
-function cnvtToCoords(i) {
-    //geocoder = new google.maps.Geocoder();
-    geocoder.geocode( {'address':allPoints[i]}, function(results, status){
-        if(status === google.maps.GeocoderStatus.OK) {
-            allPointsCoords[i] = results[0].geometry.location;
-            alert(allPointsCoords[i]);
-            if(i < allPoints.length-1)
-                cnvtToCoords(i+1);
-            else
-                maxmzeRoute();
-        }
-        else
-            alert("Geocoding end location failure: " + status);
-    });
-}
-
-function maxmzeRoute() {
-    var maxmzeWayPoints = [];
-    strtLnDist = totalCoordsDiff(allPointsCoords[0], allPointsCoords[allPointsCoords.length-1]);
-    alert("In maxmzeRoute, strtLnDist: " + strtLnDist);
-    for(i=1; i<(allPoints.length-1); i++) {
+function optmzeRoute() {
+    var optmzeWayPoints = [];
+    for(i=1; i<(allPntCoords.length-1); i++) {
         if(!skipPoint(i))
-            maxmzeWayPoints.push(allPoints[i]);
+            optmzeWayPoints.push(directionsRequest.waypoints[i]);
     }
-    directionsRequest.waypoints = maxmzeWayPoints;
+    directionsRequest.waypoints = optmzeWayPoints;
     drawCurrRoute();
 }
 
 function skipPoint(i) {
-    nextDist = totalCoordsDiff(allPointsCoords[i], allPointsCoords[i+1]);
-    skipDist = totalCoordsDiff(allPointsCoords[i-1], allPointsCoords[i+1]);
+    nextDist = totalCoordsDiff(allPntCoords[i], allPntCoords[i+1]);
+    skipDist = totalCoordsDiff(allPntCoords[i-1], allPntCoords[i+1]);
     if(skipDist < nextDist) //modify by a factor of strtLnDist
         return false;
     else
